eb989c90271365c189073900f0d59f10da531aad
scorpio686 <31187145+scorpio686@users.noreply.github.com>
2021-12-20 11:17:44 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,2 @@
 Config          = {}
 Config.Locale   = 'fr'
-
-Config.DrawDistance = 100
-Config.MarkerSize = {x = 1.1, y = 1.1, z = 0.9}
-Config.MarkerType =  21
-Config.MarkerColor = {r = 102, g = 102, b = 204, a = 255}
-
